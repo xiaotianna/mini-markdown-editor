a7ee83d8a6834e256ed9e67491395901034cd7ae
xiaotianna <127941140+xiaotianna@users.noreply.github.com>
2025-01-17 16:08:47 +0800

    test(mini-markdown-play): new mini-markdown-play project demo

--- packages/mini-markdown-material/src/components/Heading/index.tsx
+++ packages/mini-markdown-material/src/components/Heading/index.tsx
@@ -1,36 +1,48 @@
 import { FC } from 'react'
 import styled from 'styled-components'
+import { LineWrap } from '../../style/LineWrap'
 
-const HeadingStyle = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
+/**
+ *  '#' 这个字符，以及后面内容的样式（span包裹）
+ */
+const HeadingStyle = styled.span`
+  color: #3f4a54;
+  font-weight: bold;
+  .heading-tag {
+    color: #0000ff;
+  }
+`
 
 const Heading: FC<{
-  level: number,
+  level: number
   children: React.ReactNode
 }> = ({ level, children }) => {
-  switch (level) {
-    case 1:
-      return <HeadingStyle>
-        <h1># {children}</h1>
+  // switch (level) {
+  //   case 1:
+  //     return <HeadingStyle>
+  //       <h1># {children}</h1>
+  //     </HeadingStyle>
+  //   case 2:
+  //     return <h2>## {children}</h2>
+  //   case 3:
+  //     return <h3>### {children}</h3>
+  //   case 4:
+  //     return <h4>#### {children}</h4>
+  //   case 5:
+  //     return <h5>##### {children}</h5>
+  //   case 6:
+  //     return <h6>###### {children}</h6>
+  //   default:
+  //     return <></>
+  // }
+  return (
+    <LineWrap>
+      <HeadingStyle>
+        <span className='heading-tag'>{'#'.repeat(level)}</span>
       </HeadingStyle>
-    case 2:
-      return <h2>## {children}</h2>
-    case 3:
-      return <h3>### {children}</h3>
-    case 4:
-      return <h4>#### {children}</h4>
-    case 5:
-      return <h5>##### {children}</h5>
-    case 6:
-      return <h6>###### {children}</h6>
-    default:
-      return <></>
-  }
+      <HeadingStyle> {children}</HeadingStyle>
+    </LineWrap>
+  )
 }
 
-export {
-  Heading
-}
+export { Heading }
--- packages/mini-markdown-play/src/App.tsx
+++ packages/mini-markdown-play/src/App.tsx
@@ -1,26 +1,59 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Heading } from '@mini-markdown/material/src/components/Heading/index.tsx'
+import { Heading } from '@mini-markdown/material/src/components/Heading/index.tsx';
+import { useRef } from 'react';
+import ReactDOMServer from 'react-dom/server';
 
 function App() {
+  const editRef = useRef<HTMLDivElement>(null);
+  const handleClick = () => {
+    if (document.activeElement != editRef.current) {
+      editRef.current!.focus()
+    }
+    const headingHtml = ReactDOMServer.renderToStaticMarkup(<Heading level={2}>hello</Heading>);
+    document.execCommand('insertHTML', false, headingHtml);
+  };
+
+  const handleInput = (e: React.FormEvent<HTMLDivElement>) => {
+    console.log('input event', e);
+    // const selection = getSelection();
+    // if (selection && selection.rangeCount > 0) 
+    //   const range = selection.getRangeAt(0);
+    //   console.log('Current cursor position:', range);
+    // }
+  };
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <header
+        style={{
+          width: '50%',
+          border: '1px solid red',
+          margin: '0 auto',
+          padding: '0 10px'
+        }}
+        onClick={handleClick}
+      >
+        <button>h1</button>
+      </header>
+      <div
+        style={{
+          width: '50%',
+          height: '50vh',
+          border: '1px solid red',
+          margin: '0 auto',
+          padding: '10px'
+        }}
+        contentEditable={true}
+        onInput={handleInput}
+        spellCheck={false}
+        suppressContentEditableWarning={true}
+        autoFocus={true}
+        ref={editRef}
+
+      >
+        <Heading level={2}>hello</Heading>
       </div>
-      <Heading level={2}>
-        hello
-      </Heading>
-    </>
-  )
+    </div>
+  );
 }
 
-
-
-export default App
+export default App;
