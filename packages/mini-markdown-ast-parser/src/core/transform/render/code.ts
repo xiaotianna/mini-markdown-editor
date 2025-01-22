13b67d9a316591017ec5e5c13b883e55d6692b53
xiaotianna <127941140+xiaotianna@users.noreply.github.com>
2025-01-22 20:30:12 +0800

    fix(mini-markdown-ast-parser): 修复code包含范围 和 语言不正确报错
    git push

--- packages/mini-markdown-ast-parser/src/core/transform/render/code.ts
+++ packages/mini-markdown-ast-parser/src/core/transform/render/code.ts
@@ -3,11 +3,24 @@ import { Tokens } from '@/types/tokens'
 import hljs from 'highlight.js'
 
 export const renderCode = (node: Tokens) => {
-  // const escapedCodeValue = node
-  //   .value!.replace(/</g, '&lt;')
-  //   .replace(/>/g, '&gt;')
+  let language = 'plaintext'
+  try {
+    // 检查语言是否有效
+    const lang = node.lang || 'plaintext'
+    if (lang !== 'plaintext' && !hljs.getLanguage(lang)) {
+      node.lang = 'plaintext'
+    }
+    if (node.lang) {
+      hljs.highlight('', { language: node.lang })
+      language = node.lang
+    }
+  } catch (e) {
+    // 如果语言无效，使用plaintext
+    language = 'plaintext'
+  }
+
   const highlightedCode = hljs.highlight(node.value!, {
-    language: node.lang || 'plaintext',
+    language
   }).value
   return `<div class="${prefix}-code-container">
     <div class="${prefix}-code-header">
@@ -16,8 +29,8 @@ export const renderCode = (node: Tokens) => {
         <span></span>
         <span></span>
       </div>
-      <div class="${prefix}-code-right">${node.lang}</div>
+      <div class="${prefix}-code-right">${language}</div>
     </div>
-    <pre><code class="language-${node.lang}">${highlightedCode}</code></pre>
+    <pre><code class="language-${language}">${highlightedCode}</code></pre>
   </div>`
 }
