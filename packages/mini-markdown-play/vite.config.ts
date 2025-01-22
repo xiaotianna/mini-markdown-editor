e50e889e3a5b9f4ca0009ef11d8daa1ada0f324b
xiaotianna <127941140+xiaotianna@users.noreply.github.com>
2025-01-22 21:08:46 +0800

    feat(mini-markdown-play): 测试用例, css模块化方案调研

--- packages/mini-markdown-play/vite.config.ts
+++ packages/mini-markdown-play/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
-console.log(fileURLToPath(new URL('./src', import.meta.url)));
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    // alias: {
-    //   '@src': fileURLToPath(new URL('./src', import.meta.url))
-    // }
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
 })
