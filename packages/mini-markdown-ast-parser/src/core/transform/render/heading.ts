3d46ba982bf9dfb1d44523f7967f0ec5da79bbd5
xiaotianna <127941140+xiaotianna@users.noreply.github.com>
2025-01-24 14:57:03 +0800

    fix(mini-markdown-ast-parser): 修复ast解析的data-line属性没推送的bug, 新增构建命令

--- packages/mini-markdown-ast-parser/src/core/transform/render/heading.ts
+++ packages/mini-markdown-ast-parser/src/core/transform/render/heading.ts
@@ -1,9 +1,9 @@
-import { Tokens } from '@/types/tokens'
-import { astToHtml } from '..'
-import { prefix } from '@/common/constant'
+import { Tokens } from "@/types/tokens";
+import { astToHtml } from "..";
+import { prefix } from "@/common/constant";
 
 export const renderHeading = (node: Tokens) => {
-  return `<h${node.depth} class="${prefix}-h${node.depth}">${node.children?.map(astToHtml).join('')}</h${
+  return `<h${node.depth} class="${prefix}-h${node.depth}" data-line="${node.position.start.line}">${node.children?.map(astToHtml).join("")}</h${
     node.depth
-  }>`
-}
+  }>`;
+};
--- packages/mini-markdown-ast-parser/src/core/transform/render/table.ts
+++ packages/mini-markdown-ast-parser/src/core/transform/render/table.ts
@@ -1,7 +1,7 @@
-import { Tokens } from '@/types/tokens'
-import { astToHtml } from '..'
-import { prefix } from '@/common/constant'
+import { Tokens } from "@/types/tokens";
+import { astToHtml } from "..";
+import { prefix } from "@/common/constant";
 
 export const renderTable = (node: Tokens) => {
-  return `<table class="${prefix}-table">${node.children?.map(astToHtml).join('')}</table>`
-}
+  return `<table class="${prefix}-table" data-line="${node.position.start.line}">${node.children?.map(astToHtml).join("")}</table>`;
+};
