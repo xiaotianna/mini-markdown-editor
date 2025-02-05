ecfee790913af09e2e7a6101a2a4d8da4204e533
wifi歪f <1402772884@qq.com>
2025-02-06 04:16:38 +0800

    chore(mini-markdown-material): 删除无用项目

--- packages/mini-markdown-material/src/components/base/LineWrap.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-/**
- * 包裹容器，用于限制每一行内容
- */
-export const LineWrap = styled.div`
-    width: 100%;
-    height: 20px;
-`
--- packages/mini-markdown-material/src/main.tsx
+++
@@ -1,5 +0,0 @@
-// ts 类型
-export type { PropsType } from '@/components/Heading'
-
-// 组件
-export { default as Heading } from '@/components/Heading'
