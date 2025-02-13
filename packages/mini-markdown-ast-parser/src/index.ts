0869cab76fd9d716e4fa3ed89bf201694420b993
wifi歪f <1402772884@qq.com>
2025-02-13 19:00:12 +0800

    docs(mini-editor-docs): 更新文档api

--- packages/mini-markdown-ast-parser/src/index.ts
+++ packages/mini-markdown-ast-parser/src/index.ts
@@ -1,15 +1,15 @@
 // 用于将 markdown 转换为 ast
-export { parseMarkdown } from './core/parse'
+export { parseMarkdown } from "./core/parse";
 // 用于将 ast 转换为 html
-export { transformHtml } from './core/transform'
+export { transformHtml } from "./core/transform";
 // 前缀
-export { prefix } from './common/constant'
+export { prefix } from "./common/constant";
 // 分词类型
-export { TOKENS_TYPES } from './common/tokens-types'
-
+export { TOKENS_TYPES } from "./common/tokens-types";
 // 解析的 html 样式
-import './styles/index.css'
+import "./styles/index.css";
 
 // ts 类型
-export type { Tokens, RootTokens } from './types/tokens'
-export type { TokensTypes, TokensTypesKey, TokenTypeVal } from './types/tokens-types'
+export type { Tokens, RootTokens } from "./types/tokens";
+export type { TokensTypes, TokensTypesKey, TokenTypeVal } from "./types/tokens-types";
+export type { IncrementalParseOptions } from "./core/parse";
