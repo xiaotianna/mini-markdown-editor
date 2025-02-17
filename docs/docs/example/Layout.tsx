5f3b4e8d1163382f7590cbcdcdecb0a5813bc0f9
wifi歪f <1402772884@qq.com>
2025-02-17 22:09:33 +0800

    style(mini-markdown-docs): "在线尝试"样式调整

--- docs/docs/example/Layout.tsx
+++ docs/docs/example/Layout.tsx
@@ -5,7 +5,7 @@ const Layout: FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
   return (
-    <div className="w-full px-6 py-4">
+    <div className="w-full h-[90vh] px-6 py-4">
       <header className="flex justify-between items-center">
         <h1 className="text-xl font-bold">在线尝试🥳</h1>
       </header>
